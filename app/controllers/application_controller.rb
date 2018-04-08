9316cf1df6a6535452dfbd8017bb4f5072e11b36
Antonio Reid <read.musik@gmail.com>
2018-04-08 22:34:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,9 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/recipes' do 
+  post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    
+    #redirect to '/'
   end
 
   get '/recipes/:id' do
